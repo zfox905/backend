c7e7b6151a71cb0192f5b7b663f85d25a7db0822
zfox905 <76781656+zfox905@users.noreply.github.com>
2022-12-15 01:54:01 +0100

    Update Ad.java

--- src/main/java/com/developer/ads/cellads/models/Ad.java
+++ src/main/java/com/developer/ads/cellads/models/Ad.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor ;
 import lombok.Setter;
 
 import javax.persistence.Entity;
